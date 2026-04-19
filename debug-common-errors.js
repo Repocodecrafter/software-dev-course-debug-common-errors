0bf1e7ddbf32c058250904095463c082e81c952c
meghcodes <marellameghana99@gmail.com>
2026-04-19 01:08:30 -0500

    Fixed syntax, runtime, and logic errors in JavaScript practice programs

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,22 +24,29 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");   // Syntax Error: Missing closing parenthesis
 
-// What’s Wrong?
+// What’s Wrong?   Missing  ')' closing parenthesis
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
-for (let i = 0; i < numbers.length; i++) {
-  let doubled = numbers[i] * 2;
-  console.log(doubled);
+let numbers = [2, 4, "eight"];                          // Runtime & logical error: "eight" non-number value in array
+
+for (let i = 0; i < numbers.length; i++) {                         
+
+  if (typeof numbers[i] === "number") {
+    let doubled = numbers[i] * 2;
+    console.log(doubled);
+  } else {
+    console.log("Invalid number:", numbers[i]);
+  }
+
 }
 
-// What’s Wrong?
+// What’s Wrong?   "eight" non-number value in array
 
 
 
@@ -47,16 +54,16 @@ for (let i = 0; i < numbers.length; i++) {
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
-function isPrime(num) {
+function isPrime(num) {                                  // Logic Error: incorrect return values for prime checking
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
-// What’s Wrong?
+// What’s Wrong?    incorrect return values for checking prime number
